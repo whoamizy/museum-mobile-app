61ff745af000186dda9d788a4ebb59b23fc6f562
whoamizy <delovoysergei098@gmail.com>
2023-11-14 14:58:41 +0400

    feat: add fonts

---
+++ src/theme/fonts.ts
@@ -0,0 +1,6 @@
+export enum fonts {
+  montserrat400 = 'Montserrat-Regular',
+  montserrat500 = 'Montserrat-Medium',
+  montserrat600 = 'Montserrat-SemiBold',
+  montserrat700 = 'Montserrat-Bold',
+}
