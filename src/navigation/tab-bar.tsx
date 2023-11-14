f97bf382800ff7ab2e07583d954dadc98aefe661
whoamizy <delovoysergei098@gmail.com>
2023-11-14 18:52:11 +0400

    fix: tabbar label fontsize

--- src/navigation/tab-bar.tsx
+++ src/navigation/tab-bar.tsx
@@ -33,7 +33,7 @@ export const TabBar = () => {
             tabBarLabel: title,
             tabBarLabelStyle: {
               fontFamily: theme.font.montserrat500,
-              fontSize: 12,
+              fontSize: 14,
               paddingBottom: 3,
             },
           }}
