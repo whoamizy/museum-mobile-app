9e4f3789a88cc0bb78982757a9c4202c65280b3c
whoamizy <delovoysergei098@gmail.com>
2023-11-16 15:41:45 +0400

    feat: add refresher and fix paddingBottom

--- src/screens/exhibition/exhibition-screen.tsx
+++ src/screens/exhibition/exhibition-screen.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { RefreshControl, ScrollView, View } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import { useTheme } from 'styled-components/native'
 
@@ -8,6 +8,7 @@ import { Container, Loader, Separator, Wrapper } from 'src/components'
 import { usePaddingBottom } from 'src/hooks'
 import { t } from 'src/i18n'
 import { type ExhibitionProp } from 'src/navigation/types'
+import { queryClient } from 'src/utils'
 
 import { ExhibitionSlider } from './exhibition-slider'
 import {
@@ -22,12 +23,22 @@ import {
 export const ExhibitionScreen = () => {
   const paddingBottom = usePaddingBottom()
   const { red_dark } = useTheme()
+  const [refreshing, setRefreshing] = useState(false)
 
   const {
     params: { id },
   } = useRoute<ExhibitionProp>()
 
   const { data: exhibition } = useGetOneExhibition(id)
+
+  const onRefresh = () => {
+    setRefreshing(true)
+    queryClient.refetchQueries({
+      queryKey: ['exhibitions', id],
+      type: 'active',
+    })
+    setRefreshing(false)
+  }
 
   if (!exhibition) {
     return (
@@ -41,18 +52,23 @@ export const ExhibitionScreen = () => {
 
   return (
     <Wrapper>
-      <ScrollView>
-        <ExhibitionSlider images={exhibition.images} />
-        <Container>
-          <Title>{exhibition.name}</Title>
-          <Address>{exhibition.address}</Address>
-          <Price>
-            {t('exhibitions.item.price', { value: exhibition.price })}
-          </Price>
-          <Separator />
-          <Title>{t('exhibitions.item.description')}</Title>
-          <Description>{exhibition.description}</Description>
-        </Container>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <View style={{ paddingBottom }}>
+          <ExhibitionSlider images={exhibition.images} />
+          <Container>
+            <Title>{exhibition.name}</Title>
+            <Address>{exhibition.address}</Address>
+            <Price>
+              {t('exhibitions.item.price', { value: exhibition.price })}
+            </Price>
+            <Separator />
+            <Title>{t('exhibitions.item.description')}</Title>
+            <Description>{exhibition.description}</Description>
+          </Container>
+        </View>
       </ScrollView>
       <StyledButton
         title={t('exhibitions.item.getTicket')}
