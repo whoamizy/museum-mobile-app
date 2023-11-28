bfa083d08282773c60eb322a870163d6ad04b9bd
whoamizy <delovoysergei098@gmail.com>
2023-11-28 11:13:57 +0400

    feat: filter tickets

--- src/screens/tickets/index.tsx
+++ src/screens/tickets/index.tsx
@@ -1,10 +1,11 @@
-import { useLayoutEffect } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import { useTheme } from 'styled-components/native'
 
 import { useGetAllTickets } from 'src/api'
 import { Header, Loader, Wrapper } from 'src/components'
 import { t } from 'src/i18n'
 import { useNavigation } from 'src/navigation/hooks'
+import { filterTickets } from 'src/utils'
 
 import { TicketsList } from './list'
 import { LoaderWrapper } from './styles'
@@ -16,12 +17,19 @@ export const TicketsScreen = () => {
   const { setOptions } = useNavigation()
 
   const { data: tickets, isLoading } = useGetAllTickets()
+  const [filteredTickets, setFilteredTickets] = useState(tickets)
 
   useLayoutEffect(() => {
     setOptions({
       header: ScreenHeader,
     })
   }, [setOptions])
+
+  useEffect(() => {
+    if (tickets) {
+      setFilteredTickets(filterTickets(tickets))
+    }
+  }, [tickets])
 
   return (
     <Wrapper>
@@ -30,7 +38,7 @@ export const TicketsScreen = () => {
           <Loader color={red_dark} />
         </LoaderWrapper>
       )}
-      {!!tickets && <TicketsList tickets={tickets} />}
+      {!!filteredTickets && <TicketsList tickets={filteredTickets} />}
     </Wrapper>
   )
 }
